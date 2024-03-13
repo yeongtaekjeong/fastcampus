23e3b3f48605a014cfa7404291fae3fc438209b8
yeongtaekjeong <jeongyt9290@gmail.com>
2024-03-13 23:37:21 +0900

    0313

---
+++ java-lecture/src/main/java/com/fastcampus/de/java/clip14/ListExample.java
@@ -0,0 +1,37 @@
+package com.fastcampus.de.java.clip14;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class ListExample {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(5);
+        list.add(4);
+        list.add(11);
+        list.add(10);
+        System.out.println(list);
+
+        Collections.sort(list);
+        System.out.println(list);
+        System.out.println(list.size());
+
+        list.remove(4);
+        System.out.println(list);
+
+        int[] intArr = {1,5,4,11,10};
+
+        for (int i=1;i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+        for (int cur:list) {
+            System.out.println(cur);
+        }
+
+
+
+    }
+}
